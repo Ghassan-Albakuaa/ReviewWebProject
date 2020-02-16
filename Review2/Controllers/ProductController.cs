760ce67ad7b66ef78011aea65525b5cee4e398e6
ghassan <gh09an@gmail.com>
2020-02-16 04:17:06 -0500

    create repository and controller and model

---
+++ Review2/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Review2.Repositories;
+
+namespace Review2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ViewResult ProductIndex()
+        {
+
+            ProductRepository productRepo = new ProductRepository();
+
+            var model = productRepo.GetAll();
+
+            return View(model);
+        }
+    }
+}
---
+++ Review2/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Review2.Repositories
+{
+    interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+    }
+}
